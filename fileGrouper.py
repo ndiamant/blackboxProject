35b5969d2eb406b6e9d3d265b5933c6d6b362afa
Nathaniel Diamant <ndiamant@hmc.edu>
2016-06-27 16:26:43 -0700

    fixed username function in fileGrouper

--- fileGrouper.py
+++ fileGrouper.py
@@ -1,18 +1,24 @@
 import os
 import payloadReader
+import time
 
 def getTextFiles(indexDirectory, targetDirectory, targetText, userName):
 	"""
 	Assumes index files are in the format found on the white box server. 
 	"""
+	start = time.time()
+
 	for p, d, f in os.walk(indexDirectory): # path, directories, files
 		f = filter(lambda x: "index" in x, f) # make sure only using index files
 		for file in f:
 			newDir = targetDirectory + os.sep + file[6:]
 			payload = 'payload-' + file[6:]
 			os.mkdir(newDir)
-			scpCommand = 'scp ' + 'userName' + '@white.kent.ac.uk:/data/compile-inputs/' + payload + ' ' + newDir
+			scpCommand = 'scp ' + userName + '@white.kent.ac.uk:/data/compile-inputs/' + payload + ' ' + newDir
 			os.system(scpCommand)
 			indexList = payloadReader.createIndexList(file, indexDirectory)
 			payloadReader.writeByFileID(indexList, payload, newDir, newDir, True, targetText)
 			os.remove(newDir + os.sep + payload)
+
+	end = time.time()
+	print(end - start)
